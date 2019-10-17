6e2f5e77c759c46e5e7e677ca0f527e6920a5125
Michal Moravcik <miso.moravcik@gmail.com>
2019-10-17 21:22:10 +0200

    Simple web page

--- cdk/cdk-workshop-stack.ts
+++ cdk/cdk-workshop-stack.ts
@@ -1,9 +1,28 @@
-import { Construct, Stack, StackProps } from '@aws-cdk/core';
+import { CfnOutput, Construct, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
+import { Bucket } from '@aws-cdk/aws-s3';
+import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
+import { path as rootPath } from 'app-root-path';
+import { resolve } from 'path';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const webBucket = new Bucket(this, 'WebBucket', {
+      websiteIndexDocument: 'index.html'
+    });
+
+    webBucket.grantPublicAccess();
+
+    const webSource = Source.asset(resolve(rootPath, 'lib/web'));
+
+    new BucketDeployment(this, 'WebDeployment', {
+      sources: [webSource],
+      destinationBucket: webBucket
+    });
+
+    new CfnOutput(this, 'WebBucketUrl', {
+      value: webBucket.bucketWebsiteUrl
+    });
   }
 }
