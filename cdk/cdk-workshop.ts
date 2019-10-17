c40a3c0339200ef451c02d3d835cfdb699fa6337
Michal Moravcik <miso.moravcik@gmail.com>
2019-10-17 20:57:54 +0200

    User-specific stack name

--- cdk/cdk-workshop.ts
+++ cdk/cdk-workshop.ts
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import { App } from '@aws-cdk/core';
+import * as IAM from 'aws-sdk/clients/iam';
 
 import { CdkWorkshopStack } from './cdk-workshop-stack';
 
-const app = new App();
-new CdkWorkshopStack(app, 'cdk-workshop');
+new IAM().getUser((err, res) => {
+  const app = new App();
+  new CdkWorkshopStack(app, `cdk-workshop-${res.User.UserName}`);
+});
