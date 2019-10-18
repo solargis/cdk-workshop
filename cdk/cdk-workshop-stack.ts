14bd8cd2b3b31366a0f9ac7a5a3e26bc5d89646c
Michal Moravcik <miso.moravcik@gmail.com>
2019-10-18 14:34:22 +0200

    Pin API build and CDK setup

--- cdk/cdk-workshop-stack.ts
+++ cdk/cdk-workshop-stack.ts
@@ -1,11 +1,13 @@
 import { LambdaIntegration, RestApi } from '@aws-cdk/aws-apigateway';
-import { CfnOutput, Construct, Stack, StackProps } from '@aws-cdk/core';
+import { CfnOutput, Construct, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
+import { AttributeType, BillingMode, Table } from '@aws-cdk/aws-dynamodb';
 import { Code, Function, Runtime } from '@aws-cdk/aws-lambda';
 import { Bucket } from '@aws-cdk/aws-s3';
 import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
 import { path as rootPath } from 'app-root-path';
 import { resolve } from 'path';
 
+import { addCorsOptions } from './cors.utils';
 import { WebIndex } from './web-index';
 
 export interface CdkWorkshopStackProps extends StackProps {
@@ -16,6 +18,19 @@ export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props: CdkWorkshopStackProps) {
     super(scope, id, props);
 
+    // API
+
+    const imageBucket = new Bucket(this, 'ImageBucket');
+
+    const pinTable = new Table(this, 'PinTable', {
+      partitionKey: {
+        name: 'pointUrl',
+        type: AttributeType.STRING
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY
+    });
+
     const apiCode = Code.fromAsset('dist/api');
 
     const helloHandler = new Function(this, 'HelloHandler', {
@@ -24,10 +39,38 @@ export class CdkWorkshopStack extends Stack {
       handler: 'hello-lambda.handler'
     });
 
+    const pinHandler = new Function(this, 'PinHandler', {
+      code: apiCode,
+      runtime: Runtime.NODEJS_10_X,
+      handler: 'pin-lambda.handler',
+      environment: {
+        IMAGE_BUCKET: imageBucket.bucketName,
+        PIN_TABLE: pinTable.tableName
+      }
+    });
+    imageBucket.grantReadWrite(pinHandler);
+    pinTable.grantReadWriteData(pinHandler);
+
     const api = new RestApi(this, `CdkWorkshopAPI_${props.userName}`);
 
     const helloApi = api.root.addResource('hello');
     helloApi.addMethod('GET', new LambdaIntegration(helloHandler));
+
+    const pinApi = api.root.addResource('pin');
+
+    // OPTIONS /pin
+    addCorsOptions(pinApi);
+    // ANY /pin
+    pinApi.addMethod('ANY', new LambdaIntegration(pinHandler));
+
+    const pinPointApi = pinApi.addResource('{pointUrl}');
+
+    // OPTIONS /pin/{pointUrl}
+    addCorsOptions(pinPointApi);
+    // ANY /pin/{pointUrl}
+    pinPointApi.addMethod('ANY', new LambdaIntegration(pinHandler));
+
+    // WEB
 
     const webBucket = new Bucket(this, 'WebBucket', {
       websiteIndexDocument: 'index.html'
---
+++ cdk/cors.utils.ts
@@ -0,0 +1,31 @@
+import { IResource, MockIntegration, PassthroughBehavior } from '@aws-cdk/aws-apigateway';
+
+export function addCorsOptions(apiResource: IResource, ...customHeaders: string[]) {
+  apiResource.addMethod('OPTIONS', new MockIntegration({
+    integrationResponses: [{
+      statusCode: '200',
+      responseParameters: {
+        'method.response.header.Access-Control-Allow-Headers':
+          "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent"
+          + (customHeaders && customHeaders.length ? ',' + customHeaders.join(',') : '') + "'",
+        'method.response.header.Access-Control-Allow-Origin': "'*'",
+        'method.response.header.Access-Control-Allow-Credentials': "'false'",
+        'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,PATCH,DELETE'",
+      },
+    }],
+    passthroughBehavior: PassthroughBehavior.NEVER,
+    requestTemplates: {
+      "application/json": "{\"statusCode\": 200}"
+    },
+  }), {
+    methodResponses: [{
+      statusCode: '200',
+      responseParameters: {
+        'method.response.header.Access-Control-Allow-Headers': true,
+        'method.response.header.Access-Control-Allow-Methods': true,
+        'method.response.header.Access-Control-Allow-Credentials': true,
+        'method.response.header.Access-Control-Allow-Origin': true,
+      },
+    }]
+  })
+}
