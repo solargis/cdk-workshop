3a4261147a1d7638ff62bae0a696e18941669bfd
Michal Moravcik <miso.moravcik@gmail.com>
2019-10-22 16:15:49 +0200

    userName passed to stack, upgraded CDK to 1.14.0

--- cdk/cdk-workshop-stack.ts
+++ cdk/cdk-workshop-stack.ts
@@ -4,8 +4,12 @@ import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
 import { path as rootPath } from 'app-root-path';
 import { resolve } from 'path';
 
+export interface CdkWorkshopStackProps extends StackProps {
+  userName: string;
+}
+
 export class CdkWorkshopStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: CdkWorkshopStackProps) {
     super(scope, id, props);
 
     const webBucket = new Bucket(this, 'WebBucket', {
