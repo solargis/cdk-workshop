14282fbb4ba5ef4b562ad1ef6974f3c486f1e989
Michal Moravcik <miso.moravcik@gmail.com>
2019-10-22 16:05:21 +0200

    userName passed to stack, upgraded CDK to 1.14.0

--- cdk/cdk-workshop.ts
+++ cdk/cdk-workshop.ts
@@ -6,6 +6,7 @@ import * as IAM from 'aws-sdk/clients/iam';
 import { CdkWorkshopStack } from './cdk-workshop-stack';
 
 new IAM().getUser((err, res) => {
+  const userName = res.User.UserName;
   const app = new App();
-  new CdkWorkshopStack(app, `cdk-workshop-${res.User.UserName}`);
+  new CdkWorkshopStack(app, `cdk-workshop-${userName}`, { userName });
 });
