b252197c321e418c7f7fd70efa0886e884d3be4d
Michal Moravcik <miso.moravcik@gmail.com>
2021-10-29 02:44:26 +0200

    cdk - added tags, upgrade dependencies

--- cdk/cdk-workshop-stack.ts
+++ cdk/cdk-workshop-stack.ts
@@ -1,6 +1,6 @@
 import { LambdaIntegration, RestApi } from '@aws-cdk/aws-apigateway';
 import { CloudFrontWebDistribution, CloudFrontWebDistributionProps, PriceClass } from '@aws-cdk/aws-cloudfront';
-import { CfnOutput, Construct, Duration, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
+import { CfnOutput, Construct, Duration, RemovalPolicy, Stack, StackProps, Tags } from '@aws-cdk/core';
 import { AttributeType, BillingMode, Table } from '@aws-cdk/aws-dynamodb';
 import { Code, Function, LayerVersion, Runtime } from '@aws-cdk/aws-lambda';
 import { Bucket, EventType } from '@aws-cdk/aws-s3';
@@ -20,6 +20,12 @@ export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props: CdkWorkshopStackProps) {
     super(scope, id, props);
 
+    // Tags
+    const tags = Tags.of(scope);
+    tags.add('sg:language', 'TypeScript');
+    tags.add('sg:team', 'webapps');
+    tags.add('sg:app', 'CDK workshop');
+
     // API
 
     const imageBucket = new Bucket(this, 'ImageBucket');
@@ -37,13 +43,13 @@ export class CdkWorkshopStack extends Stack {
 
     const helloHandler = new Function(this, 'HelloHandler', {
       code: apiCode,
-      runtime: Runtime.NODEJS_10_X,
+      runtime: Runtime.NODEJS_14_X,
       handler: 'hello-lambda.handler'
     });
 
     const pinHandler = new Function(this, 'PinHandler', {
       code: apiCode,
-      runtime: Runtime.NODEJS_10_X,
+      runtime: Runtime.NODEJS_14_X,
       handler: 'pin-lambda.handler',
       environment: {
         IMAGE_BUCKET: imageBucket.bucketName,
@@ -55,14 +61,14 @@ export class CdkWorkshopStack extends Stack {
 
     const sharpLayer = new LayerVersion(this, `SharpLayer_${props.userName}`, {
       code: Code.fromAsset('lib/layers/sharp_layer.zip'),
-      compatibleRuntimes: [Runtime.NODEJS_10_X],
+      compatibleRuntimes: [Runtime.NODEJS_14_X],
       license: 'Apache-2.0',
-      description: 'Sharp image processing library v.0.23.1'
+      description: 'Sharp image processing library v.0.29.2'
     });
 
     const thumbnailHandler = new Function(this, 'ThumbnailHandler', {
       code: apiCode,
-      runtime: Runtime.NODEJS_10_X,
+      runtime: Runtime.NODEJS_14_X,
       handler: 'thumbnail-lambda.handler',
       layers: [sharpLayer],
       memorySize: 1536,
@@ -83,6 +89,7 @@ export class CdkWorkshopStack extends Stack {
     );
 
     const api = new RestApi(this, `CdkWorkshopAPI_${props.userName}`);
+    Tags.of(api).add('sg:public', 'true')
 
     const helloApi = api.root.addResource('hello');
     helloApi.addMethod('GET', new LambdaIntegration(helloHandler));
@@ -108,6 +115,7 @@ export class CdkWorkshopStack extends Stack {
     });
 
     webBucket.grantPublicAccess();
+    Tags.of(webBucket).add('sg:public', 'true');
 
     const webSource = Source.asset(resolve(rootPath, 'dist/web'));
 
